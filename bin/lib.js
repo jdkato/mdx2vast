0f5397f1d42c946d56445e9eadec2a4f1110d671
Joseph Kato <joseph@jdkato.io>
2025-03-11 17:08:22 -0700

    Add math support

--- bin/lib.js
+++ bin/lib.js
@@ -1,67 +1,69 @@
 // TODO: Should we use `micromark-extension-mdx` instead? It's "non-JS aware,"
-// which is what we want, but it doesn't seem to recognize non-expression 
+// which is what we want, but it doesn't seem to recognize non-expression
 // syntax (such as import statements).
-import { mdxjs } from 'micromark-extension-mdxjs'
-import { fromMarkdown } from 'mdast-util-from-markdown'
-import { mdxFromMarkdown } from 'mdast-util-mdx'
-import { toHast } from 'mdast-util-to-hast'
-import { h } from 'hastscript'
-import { toHtml } from 'hast-util-to-html'
+import { mdxjs } from "micromark-extension-mdxjs";
+import { fromMarkdown } from "mdast-util-from-markdown";
+import { mathFromMarkdown } from "mdast-util-math";
+import { math } from "micromark-extension-math";
+import { mdxFromMarkdown } from "mdast-util-mdx";
+import { toHast } from "mdast-util-to-hast";
+import { h } from "hastscript";
+import { toHtml } from "hast-util-to-html";
 
 const mdxNodes = [
-    'mdxjsEsm',
-    'mdxFlowExpression',
-    'mdxJsxFlowElement',
-    'mdxJsxTextElement',
-    'mdxTextExpression'
-]
+  "mdxjsEsm",
+  "mdxFlowExpression",
+  "mdxJsxFlowElement",
+  "mdxJsxTextElement",
+  "mdxTextExpression",
+];
 
 function isComment(source) {
-    return source.startsWith('{/*') && source.endsWith('*/}')
+  return source.startsWith("{/*") && source.endsWith("*/}");
 }
 
 function createCustomHandler(doc) {
-    return function customHandler(state, node, parent) {
-        const start = node.position.start.offset
-        const end = node.position.end.offset
+  return function customHandler(state, node, parent) {
+    const start = node.position.start.offset;
+    const end = node.position.end.offset;
 
-        const source = doc.slice(start, end)
-        if (node.type === 'mdxFlowExpression' && isComment(source)) {
-            return { type: 'comment', value: source.slice(3, -3) }
-        } else if (mdxNodes.includes(node.type)) {
-            const className = `mdxNode ${node.type}`
-            if (source.includes('\n')) {
-                return h('pre', h('code', { className }, source))
-            } else {
-                return h('code', { className }, source)
-            }
-        }
-
-        return null
+    const source = doc.slice(start, end);
+    if (node.type === "mdxFlowExpression" && isComment(source)) {
+      return { type: "comment", value: source.slice(3, -3) };
+    } else if (mdxNodes.includes(node.type)) {
+      const className = `mdxNode ${node.type}`;
+      if (source.includes("\n")) {
+        return h("pre", h("code", { className }, source));
+      } else {
+        return h("code", { className }, source);
+      }
     }
+
+    return null;
+  };
 }
 
 function toValeAST(doc) {
-    const mdast = fromMarkdown(doc, {
-        extensions: [mdxjs()],
-        mdastExtensions: [mdxFromMarkdown()]
-    })
+  const mdast = fromMarkdown(doc, {
+    extensions: [mdxjs(), math()],
+    mdastExtensions: [mdxFromMarkdown(), mathFromMarkdown()],
+  });
 
-    const customHandler = createCustomHandler(doc)
+  const customHandler = createCustomHandler(doc);
 
-    const hast = toHast(mdast, {
-        allowDangerousHtml: true,
-        passThrough: mdxNodes,
-        handlers: {
-            mdxjsEsm: customHandler,
-            mdxFlowExpression: customHandler,
-            mdxJsxFlowElement: customHandler,
-            mdxJsxTextElement: customHandler,
-            mdxTextExpression: customHandler
-        }
-    })
+  const hast = toHast(mdast, {
+    allowDangerousHtml: true,
+    passThrough: mdxNodes,
+    handlers: {
+      mdxjsEsm: customHandler,
+      mdxFlowExpression: customHandler,
+      mdxJsxFlowElement: customHandler,
+      mdxJsxTextElement: customHandler,
+      mdxTextExpression: customHandler,
+    },
+  });
 
-    return toHtml(hast)
+  return toHtml(hast);
 }
 
-export { toValeAST }
+export { toValeAST };
